0f7ae5645dd582a561b23f3e20e5c3ee694fe73a
consorcioCAP1 <omar.dcruz12@gmail.com>
2023-07-31 22:01:56 -0500

    primer commit del proyecto

---
+++ src/main/java/com/nttdata/bootcamp/bootcoinservice/BootCoinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.bootcoinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCoinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCoinServiceApplication.class, args);
+	}
+
+}
